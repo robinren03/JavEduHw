108107f79750404bb949a5318a3e8a8b2a0d7984
Ren <ryy19@mails.tsinghua.edu.cn>
2021-08-20 22:50:31 +0800

    rename ryanyu

---
+++ app/src/main/java/com/example/renyanyu/ui/login/LoginResult.java
@@ -0,0 +1,30 @@
+package com.example.renyanyu.ui.login;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+class LoginResult {
+    @Nullable
+    private LoggedInUserView success;
+    @Nullable
+    private Integer error;
+
+    LoginResult(@Nullable Integer error) {
+        this.error = error;
+    }
+
+    LoginResult(@Nullable LoggedInUserView success) {
+        this.success = success;
+    }
+    @Nullable
+    LoggedInUserView getSuccess() {
+        return success;
+    }
+
+    @Nullable
+    Integer getError() {
+        return error;
+    }
+}
---
+++ app/src/main/java/com/example/renyanyu/ui/login/data/LoginDataSource.java
@@ -0,0 +1,45 @@
+package com.example.renyanyu.ui.login.data;
+
+import com.example.renyanyu.ui.login.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+import com.example.renyanyu.ui.login.MD5Util;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            String mdPassword = MD5Util.string2MD5(password);
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            System.out.println("Got Exception");
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public Result<LoggedInUser> register(String username, String password) {
+
+        try {
+            String mdPassword = MD5Util.string2MD5(password);
+            // TODO: handle loggedInUser authentication
+            return new Result.Success<>(1);
+        } catch (Exception e) {
+            System.out.println("Got Exception");
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
