7ab4be133e8e1134e489604a0cff21a970ea12e2
Liu <yt-liu19@mails.tsinghua.edu.cn>
2021-08-30 21:08:31 +0800

    222

---
+++ app/src/main/java/com/example/renyanyu/BlankFragment3.java
@@ -0,0 +1,22 @@
+package com.example.renyanyu;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment3 extends Fragment {
+
+    public BlankFragment3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank_fragment3, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/renyanyu/MyFragmentPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.renyanyu;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+
+    private List<Fragment>fragmentList;
+    private String[] titles;
+
+
+    public MyFragmentPagerAdapter(FragmentManager fragmentManager, List<Fragment> fragmentList, String[] titles){
+        super(fragmentManager);
+        this.fragmentList = fragmentList;
+        this.titles = titles;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+
+}
