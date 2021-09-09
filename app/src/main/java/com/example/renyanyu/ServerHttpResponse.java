5dd8986e9138fbac69b3b2731947911e6fa7f7ce
任彦羽 <ryy19@mails.tsinghua.edu.cn>
2021-09-10 01:59:47 +0800

    Revert "Merge branch 'imp' into 'master'"
    This reverts merge request !12

--- app/src/main/java/com/example/renyanyu/ServerHttpResponse.java
+++ app/src/main/java/com/example/renyanyu/ServerHttpResponse.java
@@ -70,8 +70,8 @@ public class ServerHttpResponse {
             URLConnection connection = (URLConnection)url.openConnection();
             connection.setRequestProperty("Charset", "UTF-8");
             connection.setDoInput(true);
-            connection.setReadTimeout(20000);
-            connection.setConnectTimeout(20000);
+            connection.setReadTimeout(2000);
+            connection.setConnectTimeout(2000);
             connection.connect();
 
 
@@ -126,8 +126,8 @@ public class ServerHttpResponse {
             connection.setDoInput(true);//允许输入
             connection.setDoOutput(true);//允许输出
             connection.setRequestMethod("POST");//POST请求 要在获取输入输出流之前设置  否则报错
-            connection.setConnectTimeout(10000);
-            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(500);
+            connection.setReadTimeout(500);
             connection.connect();
 
             System.out.println("我被调用啦+3 " + oriUrl + "?" + data);
